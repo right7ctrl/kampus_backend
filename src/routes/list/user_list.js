30ecb04cc6b694ae027e6ea1cbf56a31541a6dca
Fatih Aydoğdu <fatih.aydogdu397@gmail.com>
2020-09-20 16:23:19 +0300

    try catchs added

--- src/routes/list/user_list.js
+++ src/routes/list/user_list.js
@@ -6,18 +6,23 @@ router.post('/', (req, res) => {
     let query = user.find({}).select('_id name username school avatar').limit(25);
 
     query.exec((err, doc) => {
-        if (err) {
-            res.status(500).json({
-                response: 2,
-                message: err
-            });
-        } else {
-            res.status(200).json({
-                response: 1,
-                total_count: doc.length,
-                items: doc
-            });
+        try {
+            if (err) {
+                res.status(500).json({
+                    response: 2,
+                    message: err
+                });
+            } else {
+                res.status(200).json({
+                    response: 1,
+                    total_count: doc.length,
+                    items: doc
+                });
+            }
+        } catch (e) {
+            res.status(502).json({ response: 2, message: e });
         }
+
     });
 
 });
