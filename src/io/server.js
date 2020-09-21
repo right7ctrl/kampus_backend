4abc1f6509bf2a37c6cf33ecf143cddb9fc1dc30
Murat Aslan <murataslanx33@gmail.com>
2020-09-21 23:15:59 +0300

    lts

--- src/io/server.js
+++ src/io/server.js
@@ -1,38 +1,60 @@
+var app = require('express')();
+var http = require('http').createServer(app);
 let io = require('socket.io').listen(http);
 const User = require('../schema/user/user');
 const dotenv = require('dotenv');
+const { ObjectId } = require('mongodb');
+const db = require('../../src/conn/db');
 dotenv.config();
 
-io.sockets.on('connect', (socket) => {
-    console.log("someone connected");
-    let userid;
+let activeCount = 0;
 
-    //authorize user to get notify by socket server
+io.sockets.on('connect', (socket) => {
+    let userid;
+    console.log(activeCount);
+
     socket.on('register', (user) => {
-        console.log(user);
-        User.findOne({ _id: "5f67563f77366967dc7f73e3" }, (err, doc) => {
-                if (err) {
-                    console.log('query_error: ' + err);
+        activeCount += 1;
+        userid = user;
+        let q = User.findOne(ObjectId(user)).select('_id name username mail created_at isOnline');
+        q.exec((err, doc) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (doc) {
+                    console.log(doc);
+                    doc.isOnline = true;
+                    doc.save();
                 } else {
-                    if (doc) {
-                        console.log(doc);
-                        doc.isOnline = true;
-                        doc.save();
-                    } else {
-                        console.log('userid is invalid');
-                    }
+                    console.log('Doc empty');
                 }
-            });
+            }
+        });
     });
 
 
     socket.on('disconnect', (data) => {
+        activeCount -= 1;
         try {
-            console.log('Disconnected: ' + data.uuid);
-
+            let q = User.findOne(ObjectId(userid)).select('_id name username mail created_at isOnline');
+            q.exec((err, doc) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    if (doc) {
+                        console.log(doc);
+                        doc.isOnline = false;
+                        doc.save();
+                    } else {
+                        console.log('Doc empty');
+                    }
+                }
+            });
         } catch (e) {
             console.log(e);
         }
+
+        console.log(activeCount);
 
     });
 
@@ -44,3 +66,5 @@ io.sockets.on('connect', (socket) => {
 http.listen(process.env.SOCKET_PORT, function () {
     console.log('listening on *:' + process.env.SOCKET_PORT);
 });
+
+module.exports = io;
