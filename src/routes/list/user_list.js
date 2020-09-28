96a9a04a5809c2c9b313eeb726b7f3a3a842cb49
Murat Aslan <murataslanx33@gmail.com>
2020-09-28 20:41:38 +0300

    err

--- src/routes/list/user_list.js
+++ src/routes/list/user_list.js
@@ -3,27 +3,31 @@ const user = require('../../schema/user/user')
 const router = require('express').Router();
 
 router.post('/', (req, res) => {
-    let query = user.find({}).select('_id name username school avatar').limit(25);
-
-    query.exec((err, doc) => {
-        try {
-            if (err) {
-                res.status(500).json({
-                    response: 2,
-                    message: err
-                });
-            } else {
-                res.status(200).json({
-                    response: 1,
-                    total_count: doc.length,
-                    items: doc
-                });
+    try {
+        let query = user.find({}).select('_id name username school avatar').limit(25);
+        query.exec((err, doc) => {
+            try {
+                if (err) {
+                    res.status(500).json({
+                        response: 2,
+                        message: err
+                    });
+                } else {
+                    res.status(200).json({
+                        response: 1,
+                        total_count: doc.length,
+                        items: doc
+                    });
+                }
+            } catch (e) {
+                res.status(502).json({ response: 2, message: e });
             }
-        } catch (e) {
-            res.status(502).json({ response: 2, message: e });
-        }
 
-    });
+        });
+    } catch (e) {
+        res.status(502).json({ response: 2, message: e });
+    }
+
 
 });
 
