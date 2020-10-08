8bafc45e4675f23b21b170eafb74bb7fc368e5e7
htuncel <tuncel.halit@hotmail.com>
2020-10-08 22:36:07 +0300

    no token crash

--- src/middlewares/auth_middleware.js
+++ src/middlewares/auth_middleware.js
@@ -1,20 +1,20 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const authMiddleware = (req, res, next) => {
-    try {
-
-        const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-        next();
-
-    } catch (e) {
-        console.log(e);
-        return res.status(401).send({
-            response: 2,
-            message: 'Auth failed',
-        });
+  try {
+    const tokenFull = req.headers.authorization?.split(" ");
+    if (!tokenFull || tokenFull.length < 2) {
+      throw "token format error";
     }
 
-
-}
+    const token = tokenFull[1];
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    next();
+  } catch (e) {
+    console.log(e);
+    return res.status(401).send({
+      response: 2,
+      message: "Auth failed",
+    });
+  }
+};
 module.exports = authMiddleware;
